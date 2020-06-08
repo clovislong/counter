a6e179648439b0c8c56c37924d4d3ce2f28e353c
clovislong <62772610+clovislong@users.noreply.github.com>
2020-06-08 15:36:38 -0600

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -6,10 +6,22 @@ let negative = document.querySelectorAll('button')[1];
 
 let tick = new Audio('tick.mp3');
 const checkbox = document.querySelector("input[type='checkbox']");
+const rainbowMode = document.querySelectorAll("input[type='checkbox']")[1];
 
 numElement.addEventListener('click', function() {
 	if (checkbox.checked) {
 		tick.play();
+	}
+	if (rainbowMode.checked) {
+		let color = '#' + Math.floor(Math.random() * 16777215).toString(16);
+		h1.style.backgroundColor = color;
+		color = '#' + Math.floor(Math.random() * 16777215).toString(16);
+		button.style.backgroundColor = color;
+		color = '#' + Math.floor(Math.random() * 16777215).toString(16);
+		negative.style.backgroundColor = color;
+		color = '#' + Math.floor(Math.random() * 16777215).toString(16);
+		let body = document.querySelector('body');
+		body.style.backgroundColor = color;
 	}
 	num = parseInt(h1.innerText);
 	num++;
